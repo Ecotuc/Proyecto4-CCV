e68dc3f6a1c0749fdc23d048c12de29226b7b045
Ecotuc <ivan.cotuc@galileo.edu>
2018-05-20 19:51:55 -0600

    Add files via upload

--- bienvenido.php
+++ bienvenido.php
@@ -9,12 +9,52 @@
 ?>
 
 <!DOCTYPE html>
-<html>
 <head>
-	<title>Bienvenido</title>
+	<title>Inicio</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="eb.css">
+	<link rel="icon" type="imgage/png" href="Russia.png" sizes="32x32">
 </head>
-<body>
-	<h2>Bienvenido: <?php echo $_SESSION['usuario'] ?></h2>
-	<a href="cerrarsesion.php">Cerrar sesion</a>
+<body class="fondo">
+	<div class="xd">
+			<div id="header">
+				<ul class="nav">
+					<li><a href="bienvenido.php">Inicio</a></li>	
+					<li>	
+						<a>Calendario</a>
+						<ul>
+							<li>
+							<a>Agregar</a>
+							<ul>
+								<li><a href="Insert_Cuenta.html">Cuentas</a></li>
+								<li><a href="Insert_Partida.php">Partidas</a></li>
+							</ul>
+							</li>
+							<li>
+								<a>Listar</a>
+								<ul>
+									<li><a href="Listar_Cuentas.php">Cuentas</a></li>
+									<li><a href="Listar_Partidas.php">Partidas</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a>Mi Quiniela</a>
+						<ul>
+							<li><a href="Libro_Diario.php">Libro Diario</a></li>
+							<li><a href="Libro_Mayor.php">Libro Mayor</a></li>
+							<li><a href="Balance.php">Balance</a></li>
+						</ul>
+					</li>
+					<li>
+						<a>Equipos</a>
+					</li>	
+				</ul>
+			</div>
+	</div>
+				<h2>Bienvenido: <?php echo $_SESSION['usuario'] ?></h2>
+			    <input type="submit" name="submit" value="Ingresar" class="btn-enviar" required><br>
+				<a href="cerrarsesion.php">Cerrar sesion</a>
 </body>
 </html>
